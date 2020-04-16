f200b95b84cc4537cdc7274c24bbb2cf0a768b6b
netebakari <itsumo.itsumo.nemui@gmail.com>
2020-04-16 23:47:49 +0900

    ログ設定はAPIから取得できることに気付いた

--- lib.py
+++ lib.py
@@ -6,33 +6,47 @@ import re
 def load_config():
     return yaml.load(open("config.yaml", "r", encoding="utf-8"), Loader=yaml.SafeLoader)
     
-# ALBの存在チェック
-def exist_alb(name):
+# 指定した名前のALBが存在し、ログの保存が有効になっていたらS3の情報を返す。それ以外の場合はNoneを返す
+def get_alb(name):
     client = boto3.client('elbv2')
     try:
-        client.describe_load_balancers(Names=[name])
-        return True
-    except:
-        return False
+        alb = client.describe_load_balancers(Names=[name])
+        arn = alb["LoadBalancers"][0]["LoadBalancerArn"]
+        attributes = client.describe_load_balancer_attributes(LoadBalancerArn=arn)
 
-# ELBの存在チェック
-def exist_elb(name):
+        bucket = [x for x in attributes["Attributes"] if x["Key"] == "access_logs.s3.bucket"]
+        prefix = [x for x in attributes["Attributes"] if x["Key"] == "access_logs.s3.prefix"]
+        if len(bucket) == 0 or len(prefix) == 0:
+            return None
+
+        return {"type": "ALB", "bucket": bucket[0]["Value"], "prefix": prefix[0]["Value"]}
+    except:
+        return None
+
+# 指定した名前のClassic ELBが存在し、ログの保存が有効になっていたらS3の情報を返す。それ以外の場合はNoneを返す
+def get_elb(name):
     client = boto3.client('elb')
     try:
-        client.describe_load_balancers(LoadBalancerNames=[name])
-        return True
-    except:
-        return False
+        attributes = client.describe_load_balancer_attributes(LoadBalancerName=name)
+        access_log = attributes["LoadBalancerAttributes"]["AccessLog"]
+        if not access_log["Enabled"]:
+            return None
 
-# ALBまたはELBが存在するなら文字列を、そうでなければ例外を送出
+        return {"type": "ELB", "bucket": access_log["S3BucketName"], "prefix": access_log["S3BucketPrefix"]}
+    except:
+        return None
+
+# ALBまたはELBが存在し、ログの保存が有効になっていたらその情報を返す。そうでなければ例外を送出
 def get_loadbalancer(name):
-    if exist_alb(name):
-        return "ALB"
-    
-    if exist_elb(name):
-        return "ELB"
-    
-    raise Exception(f"{name} not found")
+    alb = get_alb(name)
+    if alb != None:
+        return alb
+
+    elb = get_elb(name)    
+    if elb != None:
+        return elb
+
+    raise Exception(f"{name} not found or access log not enabled")
 
 # Athenaでクエリを実行する。スキーマ名、結果の転送先は config.yaml で指定する
 def exec_athena_query(query):
@@ -56,7 +70,7 @@ def exec_athena_query(query):
 
 # 指定されたパスにアクセスできて、オブジェクトが1個以上存在するならTrue
 def get_some_objects_from_s3(s3path):
-    match = re.search(r"s3://(?P<bucket>[^/]+)(?P<prefix>/?.*)", s3path)
+    match = re.search(r"s3://(?P<bucket>[^/]+)/?(?P<prefix>.*)", s3path)
     if match == None:
         raise Exception(f"malformed s3 path: {s3path}")
     
@@ -64,7 +78,7 @@ def get_some_objects_from_s3(s3path):
     prefix = match.group("prefix")
     if prefix == "/":
         prefix = ""
-
+    
     try:
         client = boto3.client('s3')
         objects = client.list_objects_v2(Bucket=bucket_name, MaxKeys=1, Prefix=prefix)
@@ -72,22 +86,55 @@ def get_some_objects_from_s3(s3path):
     except:
         return False
 
+
+# AWSアカウント番号を取得する
+def get_account_no():
+    client = boto3.client('sts')
+    identity = client.get_caller_identity()
+    return identity["Account"]
+
+
+# テーブル作成DDLを取得する
+def get_table_creation_ddl(type, tablename, bucket, prefix):
+    filename = "ddl_alb" if type == "ALB" else "ddl_elb"
+    file = open(f"{filename}.txt")
+    content = file.read()
+    file.close()
+    account_no = get_account_no()
+    region = load_config()["region"]
+
+    content = content.replace("$TABLENAME", tablename)
+    if prefix == "":
+        prefix = "/"
+    if prefix[0] != "/":
+        prefix = "/" + prefix
+    content = content.replace("$S3PATH", f"s3://{bucket}{prefix}/AWSLogs/{account_no}/elasticloadbalancing/{region}")
+    return content
+
+# Classic ELBのテーブルからビューを作るDDLを取得する
+def get_transformed_view_ddl(tablename):
+    file = open("ddl_elb_view.txt")
+    content = file.read()
+    file.close()
+    content = content.replace("$TABLENAME", tablename)
+    return content
+
 # 使い方を出力
 def print_usage():
     this_year = timestamp = datetime.datetime.now().year
 
     print(f"[USAGE]")
-    print(f"table.py <lb-name> <s3path> [year(={this_year})] [dryRunMode]")
+    print(f"table.py <lb-name> [year(={this_year})] [dryRunMode]")
     print(f"")
     print(f"[example 1]")
-    print(f"table.py myAlb s3://.../ap-northeast-1/")
+    print(f"table.py myAlb")
     print(f"  - create athena table for this year({this_year})")
     print(f"")
     print(f"[example 2]")
-    print(f"table.py myAlb s3://.../ap-northeast-1/ 2019")
+    print(f"table.py myAlb 2019")
     print(f"  - create athena table for year=2019")
     print(f"")
     print(f"[example 2]")
-    print(f"table.py myAlb s3://.../ap-northeast-1/ 2020 1")
+    print(f"table.py myAlb 2020 1")
     print(f"  - dry-run mode (just print DDL)")
     print(f"")
--- table.py
+++ table.py
@@ -2,37 +2,28 @@ import lib
 import sys
 import datetime
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     lib.print_usage()
     sys.exit(1)
 
 this_year = timestamp = datetime.datetime.now().year
 lb_name = sys.argv[1]
-s3path = sys.argv[2]
-year = this_year if len(sys.argv) <= 3 else int(sys.argv[3])
-dry_run_mode = len(sys.argv) >= 5
+year = this_year if len(sys.argv) <= 2 else int(sys.argv[2])
+dry_run_mode = len(sys.argv) >= 4
 
-# まずS3パスの検証を行う。1個以上オブジェクトがあるかどうかを調べる
-if not lib.get_some_objects_from_s3(s3path):
-    raise Exception(f"no object found or bucket: {s3path}")
 
 # ALBかclassic ELBかをチェック（見つからなければ例外で終わり）
-lb_type = lib.get_loadbalancer(lb_name)
-query = get_table_creation_ddl(lb_type, f"#{lb_name}_accesslogs_#{year}", s3path)
-print(query)
+loadbalancer = lib.get_loadbalancer(lb_name)
+table_name = f"{lb_name}_accesslogs_{year}"
 
-def get_table_creation_ddl(type, tablename, s3path):
-    filename = "ddl_alb" if type == "ALB" else "ddl_elb"
-    file = open(f"{filename}.txt")
-    content = file.read()
-    file.close()
-    content = content.replace("$TABLENAME", tablename)
-    content = content.replace("$S3PATH", s3path)
-    return content
+if dry_run_mode:
+    print("--------------------")
+    query = lib.get_table_creation_ddl(loadbalancer["type"], table_name, loadbalancer["bucket"], loadbalancer["prefix"])
+    print(query)
+    if loadbalancer["type"] == "ELB":
+        print("--------------------")
+        query = lib.get_transformed_view_ddl(table_name)
+        print(query)
+    print("--------------------")
+    sys.exit(0)
 
-def get_transformed_view_ddl(tablename):
-    file = open("ddl_elb_view.txt")
-    content = file.read()
-    file.close()
-    content = content.replace("$TABLENAME", tablename)
-    return content
